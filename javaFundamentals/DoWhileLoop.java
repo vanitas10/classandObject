1e87c98bed4a60b6fed697e060c56c5688fe3deb
Manuel Pecaoco III <92811949+vanitas10@users.noreply.github.com>
2023-09-21 21:52:55 +0800

    Add files via upload

---
+++ javaFundamentals/DoWhileLoop.java
@@ -0,0 +1,18 @@
+package javaFundamentals;
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		String hello[] = {"Manuel","Cadalig","Pecaoco","III"};
+		int i = 0;
+		do {
+			
+			System.out.println(hello[i]);
+			i++;
+		}while (i<hello.length);
+		
+		//System.out.println();
+
+	}
+
+}
---
+++ javaFundamentals/WhileLoop.java
@@ -0,0 +1,16 @@
+package javaFundamentals;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		String hello[] = {"Manuel","Cadalig","Pecaoco","III"};
+		int i = 0;
+		
+		while(i < hello.length) {
+			System.out.println(hello[i]);
+			i++;
+		}
+
+	}
+
+}
